feb9079715cdaac39860ce676e45905c0e22cf5e
yurannhassengo <yurannhassengo@gmail.com>
2024-09-25 02:04:42 -0700

    update Table

--- src/components/table/Item.jsx
+++ src/components/table/Item.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Item = () => {
+const Item = ({disable, produto:{id,name,qt,qa,motivo} }) => {
+
+        const [qAtual,setQAtual] = useState(qa)
+
   return (
     <div className="border-2 border-black">
       <ul className="grid grid-cols-8 justify-center space-x-8 items-center">
-        <li className='w-auto border-l-2 border-black'>Id</li>
-        <li className="col-span-3 border-l-4 ">Producto</li>
-        <li><input className="border-l-4" type="text"  value="Quantidade Atual" /></li>
-        <li><input className="border-l-4"  type="text" value="Quantidade Existente" /></li>
-        <li className="border-l-4  justify-center items-center">
-            <select className='justify-center items-center' name="" id="">
+        <li className='w-auto border-l-2 border-black'>{id}</li>
+        <li className="col-span-3 border-l-4 ">{name}</li>
+        <li className='justify-center items-center border-l-4'>
+            {qt}
+        </li>
+        <li >
+            <input onChange={(e)=>setQAtual(e.target.value)} className={`border-l-4  w-full ${!disable ? "hover:border-blue-700" : ""}`} 
+            disabled={disable} type="text" value={qAtual} placeholder="Quantidade Existente" />
+        </li>
+        <li className="border-l-4 w-full justify-center items-center">
+            <select className=' justify-center items-center' disabled={disable} name="" id="">
                 <option value="">Selecione...</option>
-                <option value="opcao1">Falha de Registo</option>
-                <option value="opcao2">Quebra de Stock</option>
-                <option value="opcao3">Acerto de stock</option>
-                <option value="opcao4">Outros</option>
+                <option value="falha">Falha de Registo</option>
+                <option value="quebra">Quebra de Stock</option>
+                <option value="acerto">Acerto de stock</option>
+                <option value="outros">Outros</option>
             </select> 
             </li>
       </ul>
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Item from './Item'
 
 const Table = () => {
+
+    const [editar,setEditar] = useState(false);
+
+    const produtos = [
+        {
+          id:1,
+          name:"Bernina",
+          qt:10,
+          qa:10,
+          motivo:""
+        },
+        {
+          id:1,
+          name:"2M Lata",
+          qt:10,
+          qa:10,
+          motivo:""
+        },
+        {
+          id:1,
+          name:"Heineken Mini",
+          qt:10,
+          qa:10,
+          motivo:""
+        },
+        {
+          id:1,
+          name:"Castle Lite",
+          qt:10,
+          qa:10,
+          motivo:""
+        },
+    ]
+
   return (
     <div>
-      <Item/>
-      <Item/>
-      <Item/>
+      <div className='flex justify-center items-start'>
+            <button onClick={()=>setEditar(!editar)} className='w-auto border-2 px-2 text-white border-blue-300 bg-blue-400 rounded'>
+                {editar?"Editar":"Salvar"}
+            </button>
+      </div>
+      <div>
+
+        {produtos.map( item => { return <Item produto={item} disable={editar}/> } )}
+        
+        
+      </div>
     </div>
   )
 }
